1a1c778a3eb49f9ec52cbc145f30f0552050d878
Swetha B S <swetha@multicorewareinc.com>
2021-12-01 19:05:32 +0000

    Add test suite rpp-performancetests support for RICAP HOST pk3,pln1,pln3

--- utilities/rpp-performancetests/HOST_NEW/BatchPD_host_pln3.cpp
+++ utilities/rpp-performancetests/HOST_NEW/BatchPD_host_pln3.cpp
@@ -22,6 +22,25 @@ typedef half Rpp16f;
 
 #define RPPPIXELCHECK(pixel) (pixel < (Rpp32f)0) ? ((Rpp32f)0) : ((pixel < (Rpp32f)255) ? pixel : ((Rpp32f)255))
 
+void swap (unsigned int *a, unsigned int *b)
+{
+    unsigned int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void randomize (unsigned int arr[], unsigned int n)
+{
+    // Use a different seed value each time
+    srand (time(NULL));
+    for (unsigned int i = n - 1; i > 0; i--)
+    {
+        // Pick a random index from 0 to i
+        unsigned int j = rand() % (i + 1);
+        swap(&arr[i], &arr[j]);
+    }
+}
+
 int main(int argc, char **argv)
 {
     const int MIN_ARG_COUNT = 7;
@@ -29,7 +48,7 @@ int main(int argc, char **argv)
     if (argc < MIN_ARG_COUNT)
     {
         printf("\nImproper Usage! Needs all arguments!\n");
-        printf("\nUsage: ./BatchPD_host_pln3 <src1 folder> <src2 folder (place same as src1 folder for single image functionalities)> <u8 = 0 / f16 = 1 / f32 = 2 / u8->f16 = 3 / u8->f32 = 4 / i8 = 5 / u8->i8 = 6> <outputFormatToggle (pkd->pkd = 0 / pkd->pln = 1)> <case number = 0:81> <verbosity = 0/1>\n");
+        printf("\nUsage: ./BatchPD_host_pln3 <src1 folder> <src2 folder (place same as src1 folder for single image functionalities)> <u8 = 0 / f16 = 1 / f32 = 2 / u8->f16 = 3 / u8->f32 = 4 / i8 = 5 / u8->i8 = 6> <outputFormatToggle (pkd->pkd = 0 / pkd->pln = 1)> <case number = 0:82> <verbosity = 0/1>\n");
         return -1;
     }
 
@@ -40,7 +59,7 @@ int main(int argc, char **argv)
         printf("\nsrc2 = %s", argv[2]);
         printf("\nu8 / f16 / f32 / u8->f16 / u8->f32 / i8 / u8->i8 (0/1/2/3/4/5/6) = %s", argv[3]);
         printf("\noutputFormatToggle (pkd->pkd = 0 / pkd->pln = 1) = %s", argv[4]);
-        printf("\ncase number (1:7) = %s", argv[5]);
+        printf("\ncase number (1:82) = %s", argv[5]);
     }
 
     char *src = argv[1];
@@ -366,6 +385,9 @@ int main(int argc, char **argv)
         break;
     case 81:
         strcpy(funcName, "color_jitter");
+        break;
+    case 82:
+        strcpy(funcName, "ricap");
         break;
     default:
         strcpy(funcName, "test_case");
@@ -3667,6 +3689,73 @@ int main(int argc, char **argv)
             test_case_name = "color_jitter";
             missingFuncFlag = 1;
         }
+        case 82:
+    {
+        test_case_name = "ricap";
+        Rpp32u initialPermuteArray[images];
+        Rpp32u permutedArray1[images] ;
+        Rpp32u permutedArray2[images] ;
+        Rpp32u permutedArray3[images] ;
+        Rpp32u permutedArray4[images] ;
+        Rpp32u cropRegion1[4] ;
+        Rpp32u cropRegion2[4] ;
+        Rpp32u cropRegion3[4] ;
+        Rpp32u cropRegion4[4] ;
+        for (uint i = 0; i < images; i++ )
+        {
+            initialPermuteArray[i] = i;
+        }
+        randomize(initialPermuteArray, images);
+        memcpy(permutedArray1, initialPermuteArray, images * sizeof(Rpp32u));
+        randomize(initialPermuteArray, images);
+        memcpy(permutedArray2, initialPermuteArray, images * sizeof(Rpp32u));
+        randomize(initialPermuteArray, images);
+        memcpy(permutedArray3, initialPermuteArray, images * sizeof(Rpp32u));
+        randomize(initialPermuteArray, images);
+        memcpy(permutedArray4, initialPermuteArray, images * sizeof(Rpp32u));
+
+        cropRegion1[0] = 3; //x1
+        cropRegion1[1] = 17;    //y1
+        cropRegion1[2] = 250;   //w1
+        cropRegion1[3] = 238;   //h1
+
+        cropRegion2[0] = 7;  //x2
+        cropRegion2[1] = 16;  //y2
+        cropRegion2[2] = 50;  //w2
+        cropRegion2[3] = 238; //h2
+
+        cropRegion3[0] = 7;   //x3
+        cropRegion3[1] = 106; //y3
+        cropRegion3[2] = 250; //w3
+        cropRegion3[3] = 62;  //h3
+
+        cropRegion4[0] = 103; //x4
+        cropRegion4[1] = 12;  //y4
+        cropRegion4[2] = 50;  //w4
+        cropRegion4[3] = 62;  //h4
+        start_omp = omp_get_wtime();
+        start = clock();
+        if (ip_bitDepth == 0)
+            rppi_ricap_u8_pln3_batchPD_host(input, srcSize, maxSize, output, permutedArray1, permutedArray2, permutedArray3, permutedArray4, cropRegion1, cropRegion2, cropRegion3, cropRegion4, outputFormatToggle, noOfImages, handle);
+        else if (ip_bitDepth == 1)
+            rppi_ricap_f16_pln3_batchPD_host(inputf16, srcSize, maxSize, outputf16,  permutedArray1, permutedArray2, permutedArray3, permutedArray4, cropRegion1, cropRegion2, cropRegion3, cropRegion4, outputFormatToggle, noOfImages, handle);
+        else if (ip_bitDepth == 2)
+            rppi_ricap_f32_pln3_batchPD_host(inputf32, srcSize, maxSize, outputf32, permutedArray1, permutedArray2, permutedArray3, permutedArray4, cropRegion1, cropRegion2, cropRegion3, cropRegion4, outputFormatToggle, noOfImages, handle);
+        else if (ip_bitDepth == 3)
+            missingFuncFlag = 1;
+        else if (ip_bitDepth == 4)
+            missingFuncFlag = 1;
+        else if (ip_bitDepth == 5)
+            rppi_ricap_i8_pln3_batchPD_host(inputi8, srcSize, maxSize, outputi8, permutedArray1, permutedArray2, permutedArray3, permutedArray4, cropRegion1, cropRegion2, cropRegion3, cropRegion4, outputFormatToggle, noOfImages, handle);
+        else if (ip_bitDepth == 6)
+            missingFuncFlag = 1;
+        else
+            missingFuncFlag = 1;
+        end = clock();
+        end_omp = omp_get_wtime();
+
+        break;
+    }
         default:
             missingFuncFlag = 1;
             break;
