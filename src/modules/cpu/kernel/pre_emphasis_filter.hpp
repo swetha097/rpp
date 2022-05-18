41d12161f95180bab5b4ba658b9c1149461d80b5
sampath1117 <sampath.rachumallu@multicorewareinc.com>
2022-05-18 17:37:14 +0000

    minor changes

--- src/modules/cpu/kernel/pre_emphasis_filter.hpp
+++ src/modules/cpu/kernel/pre_emphasis_filter.hpp
@@ -13,18 +13,20 @@ RppStatus pre_emphasis_filter_host_tensor(Rpp32f *srcPtr,
 #pragma omp parallel for num_threads(srcDescPtr->n)    
     for(int batchCount = 0; batchCount < srcDescPtr->n; batchCount++)
     {
+      Rpp32f *srcPtrTemp = srcPtr + batchCount * srcDescPtr->strides.nStride;
+      Rpp32f *dstPtrTemp = dstPtr + batchCount * srcDescPtr->strides.nStride;
       Rpp32s srcSize = srcSizeTensor[batchCount];
       Rpp32f coeff = coeffTensor[batchCount];
 
       if(borderType == RpptAudioBorderType::Zero)
-        dstPtr[0] = srcPtr[0];
+        dstPtrTemp[0] = srcPtrTemp[0];
       else if(borderType == RpptAudioBorderType::Clamp)
-        dstPtr[0] = srcPtr[0] * (1 - coeff); 
+        dstPtrTemp[0] = srcPtrTemp[0] * (1 - coeff); 
       else if(borderType == RpptAudioBorderType::Reflect)
-        dstPtr[0] = srcPtr[0] - coeff * srcPtr[1]; 
+        dstPtrTemp[0] = srcPtrTemp[0] - coeff * srcPtrTemp[1]; 
 
       for(int i = 1; i < srcSize; i++)
-        dstPtr[i] = srcPtr[i] - coeff * srcPtr[i - 1];
+        dstPtrTemp[i] = srcPtrTemp[i] - coeff * srcPtrTemp[i - 1];
     }
     return RPP_SUCCESS;
 }
