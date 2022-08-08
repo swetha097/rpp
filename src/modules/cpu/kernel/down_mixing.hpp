eee26bce36789d68379891ead62d1a5b9a72870f
sampath1117 <sampath.rachumallu@multicorewareinc.com>
2022-08-08 18:29:54 +0000

    added vectorized implementation for downmixing and to_decibels

--- src/modules/cpu/kernel/down_mixing.hpp
+++ src/modules/cpu/kernel/down_mixing.hpp
@@ -1,6 +1,13 @@
 #include "rppdefs.h"
 #include <omp.h>
 
+float sum4(__m128 x)
+{
+    const __m128 hsum_0 = _mm_hadd_ps(x, x);
+    const __m128 hsum_1 = _mm_hadd_ps(hsum_0, hsum_0);
+    return _mm_cvtss_f32(hsum_1);
+}
+
 RppStatus down_mixing_host_tensor(Rpp32f *srcPtr,
                                   RpptDescPtr srcDescPtr,
                                   Rpp32f *dstPtr,
@@ -39,12 +46,29 @@ RppStatus down_mixing_host_tensor(Rpp32f *srcPtr,
             weights = normalizedWeights;
         }
 
+        int channelIncrement = 4;
+		int alignedChannels = (channels / 4) * 4;
+
         // use weights to downmix to mono
-        for(int64_t dstIdx = 0, srcIdx = 0; dstIdx < samples; dstIdx++, srcIdx += channels)
+        for(int64_t dstIdx = 0; dstIdx < samples; dstIdx++)
         {
-            dstPtrTemp[dstIdx] = 0.0;
-            for(int c = 0; c < channels; c++)
-                dstPtrTemp[dstIdx] += srcPtrTemp[srcIdx + c] * weights[c];
+            __m128 pDst = _mm_setzero_ps();
+            int channelLoopCount = 0;
+            for(; channelLoopCount < alignedChannels; channelLoopCount += channelIncrement)
+            {
+                __m128 pSrc, pWeights;
+                pWeights = _mm_setr_ps(weights[channelLoopCount], weights[channelLoopCount + 1], weights[channelLoopCount + 2], weights[channelLoopCount + 3]);
+                pSrc = _mm_loadu_ps(srcPtrTemp);
+                pSrc = _mm_mul_ps(pSrc, pWeights);
+                pDst = _mm_add_ps(pDst, pSrc);
+                srcPtrTemp += channelIncrement;
+            }
+            dstPtrTemp[dstIdx] = sum4(pDst);
+            for(; channelLoopCount < channels; channelLoopCount++)
+            {
+                dstPtrTemp[dstIdx] += ((*srcPtrTemp) * weights[channelLoopCount]);
+                srcPtrTemp++;
+            }
         }
     }
 
--- src/modules/cpu/kernel/to_decibels.hpp
+++ src/modules/cpu/kernel/to_decibels.hpp
@@ -1,5 +1,6 @@
 #include "rppdefs.h"
-#include <omp.h>
+#include "rpp_cpu_simd.hpp"
+#include "rpp_cpu_common.hpp"
 
 RppStatus to_decibels_host_tensor(Rpp32f *srcPtr,
                                   RpptDescPtr srcDescPtr,
@@ -16,6 +17,12 @@ RppStatus to_decibels_host_tensor(Rpp32f *srcPtr,
     Rpp32f minRatio = std::pow(10, cutOffDB / multiplier);
     if(minRatio == 0.0f)
         minRatio = std::nextafter(0.0f, 1.0f);
+
+    Rpp32f log10Factor = 1 / std::log(10);
+    multiplier *= log10Factor;
+
+    __m256 pMultiplier = _mm256_set1_ps(multiplier);
+    __m256 pMinRatio = _mm256_set1_ps(minRatio);
 
     omp_set_dynamic(0);
 #pragma omp parallel for num_threads(srcDescPtr->n)
@@ -34,8 +41,26 @@ RppStatus to_decibels_host_tensor(Rpp32f *srcPtr,
             referenceMagnitude = 1.0;
 
         Rpp32f invReferenceMagnitude = 1.f / referenceMagnitude;
-        for(int i = 0 ; i < bufferLength ; i++)
-            dstPtrTemp[i] = multiplier * std::log10(std::max(minRatio, srcPtrTemp[i] * invReferenceMagnitude));
+        __m256 pinvMag = _mm256_set1_ps(invReferenceMagnitude);
+
+        int vectorIncrement = 8;
+		int alignedLength = (bufferLength / 8) * 8;
+		int vectorLoopCount = 0;
+		for(; vectorLoopCount < alignedLength; vectorLoopCount += vectorIncrement)
+		{
+            __m256 pSrc;
+            pSrc = _mm256_loadu_ps(srcPtrTemp);
+            pSrc = _mm256_mul_ps(pSrc, pinvMag);
+            pSrc = _mm256_max_ps(pMinRatio, pSrc);
+            pSrc = log_ps(pSrc);
+            pSrc = _mm256_mul_ps(pSrc, pMultiplier);
+            _mm256_storeu_ps(dstPtrTemp, pSrc);
+			srcPtrTemp += vectorIncrement;
+			dstPtrTemp += vectorIncrement;
+        }
+
+        for(; vectorLoopCount < bufferLength; vectorLoopCount++)
+            dstPtrTemp[vectorLoopCount] = multiplier * std::log(std::max(minRatio, srcPtrTemp[vectorLoopCount] * invReferenceMagnitude));
     }
 
     return RPP_SUCCESS;
