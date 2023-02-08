431659a1446a695cfa247ad7a962a87588d50628
sampath1117 <sampath.rachumallu@multicorewareinc.com>
2023-02-08 07:12:15 -0800

    minor change

--- src/modules/cpu/kernel/pre_emphasis_filter.hpp
+++ src/modules/cpu/kernel/pre_emphasis_filter.hpp
@@ -45,7 +45,12 @@ RppStatus pre_emphasis_filter_host_tensor(Rpp32f *srcPtr,
         }
 
         for(; vectorLoopCount < bufferLength; vectorLoopCount++)
+        {
             *dstPtrTemp = *srcPtrTemp - coeff * (*(srcPtrTemp - 1));
+            srcPtrTemp++;
+            dstPtrTemp++;
+        }
+
     }
 
     return RPP_SUCCESS;
