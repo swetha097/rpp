eb715510cb5eb04d207d84db5ab86a100d83c587
sampath1117 <sampath.rachumallu@multicorewareinc.com>
2023-02-17 14:24:53 +0000

    updated non silent region algorithm

--- src/modules/cpu/kernel/non_silent_region_detection.hpp
+++ src/modules/cpu/kernel/non_silent_region_detection.hpp
@@ -7,19 +7,17 @@ Rpp32f getSquare(Rpp32f &value)
     return (res * res);
 }
 
-Rpp32f getMax(std::vector<float> &values, int length)
+Rpp32f getMax(std::vector<float> &values, Rpp32s srcLength)
 {
     Rpp32f max = values[0];
-    for(int i = 1; i < length; i++)
-    {
+    for(int i = 1; i < srcLength; i++)
         max = std::max(max, values[i]);
-    }
     return max;
 }
 
 RppStatus non_silent_region_detection_host_tensor(Rpp32f *srcPtr,
                                                   RpptDescPtr srcDescPtr,
-                                                  Rpp32s *srcSizeTensor,
+                                                  Rpp32s *srcLengthTensor,
                                                   Rpp32f *detectedIndexTensor,
                                                   Rpp32f *detectionLengthTensor,
                                                   Rpp32f cutOffDB,
@@ -32,31 +30,33 @@ RppStatus non_silent_region_detection_host_tensor(Rpp32f *srcPtr,
     for(int batchCount = 0; batchCount < srcDescPtr->n; batchCount++)
     {
         Rpp32f *srcPtrTemp = srcPtr + batchCount * srcDescPtr->strides.nStride;
-        Rpp32s srcSize = srcSizeTensor[batchCount];
+        Rpp32s srcLength = srcLengthTensor[batchCount];
         bool referenceMax = (referencePower == 0.0f);
 
         // set reset interval based on the user input
-        Rpp32s resetLength = (resetInterval == -1) ? srcSize : resetInterval;
+        Rpp32s resetLength = (resetInterval == -1) ? srcLength : resetInterval;
 
         // Calculate buffer size for mms array and allocate mms buffer
-        Rpp32s mmsBufferSize = srcSize - windowLength + 1;
+        Rpp32s mmsBufferSize = srcLength;
         std::vector<float> mmsBuffer;
         mmsBuffer.reserve(mmsBufferSize);
 
-        // Calculate moving mean square of input array and store in mms buffer
+        // Calculate moving mean square of input array and store srcPtrTemp mms buffer
         Rpp32f meanFactor = 1.0f / windowLength;
-        int windowBegin = 0;
-        while(windowBegin <= srcSize - windowLength)
+        Rpp64s windowBegin = -windowLength + 1;
+        for (Rpp64s outPos = 0; outPos < srcLength;)
         {
             Rpp32f sumOfSquares = 0.0f;
-            for(int i = windowBegin; i < windowBegin + windowLength; i++)
+            for (Rpp64s i = std::max<Rpp64s>(windowBegin, 0); i < outPos; i++)
                 sumOfSquares += getSquare(srcPtrTemp[i]);
-            mmsBuffer[windowBegin] = sumOfSquares * meanFactor;
-            auto interval_endIdx = std::min(windowBegin + resetLength, srcSize) - windowLength + 1;
-            for(windowBegin++; windowBegin < interval_endIdx; windowBegin++)
+
+            Rpp64s intervalEndIdx = std::min<Rpp64s>(srcLength, outPos + resetLength);
+            for (; outPos < intervalEndIdx; outPos++, windowBegin++)
             {
-                sumOfSquares += getSquare(srcPtrTemp[windowBegin + windowLength - 1]) - getSquare(srcPtrTemp[windowBegin - 1]);
-                mmsBuffer[windowBegin] = sumOfSquares * meanFactor;
+                sumOfSquares += getSquare(srcPtrTemp[outPos]);
+                mmsBuffer[outPos] = sumOfSquares * meanFactor;
+                if (windowBegin >= 0)
+                    sumOfSquares -= getSquare(srcPtrTemp[windowBegin]);
             }
         }
 
@@ -65,8 +65,9 @@ RppStatus non_silent_region_detection_host_tensor(Rpp32f *srcPtr,
         Rpp32f cutOffMag = base * std::pow(10.0f, cutOffDB * 0.1f);
 
         // Calculate begining index, length of non silent region from the mms buffer
-        int endIdx = mmsBufferSize;
-        int beginIdx = endIdx;
+        Rpp32s endIdx = mmsBufferSize;
+        Rpp32s beginIdx = endIdx;
+        Rpp32s detectBegin, detectEnd;
         for(int i = 0; i < endIdx; i++)
         {
             if(mmsBuffer[i] >= cutOffMag)
@@ -75,11 +76,10 @@ RppStatus non_silent_region_detection_host_tensor(Rpp32f *srcPtr,
                 break;
             }
         }
-
         if(beginIdx == endIdx)
         {
-            detectedIndexTensor[batchCount] = 0;
-            detectionLengthTensor[batchCount] = 0;
+            detectBegin = 0;
+            detectEnd = 0;
         }
         else
         {
@@ -91,13 +91,20 @@ RppStatus non_silent_region_detection_host_tensor(Rpp32f *srcPtr,
                     break;
                 }
             }
-            detectedIndexTensor[batchCount] = beginIdx;
-            detectionLengthTensor[batchCount] = endIdx - beginIdx + 1;
+            detectBegin = beginIdx;
+            detectEnd = endIdx - beginIdx + 1;
         }
 
         // Extend non silent region
-        if(detectionLengthTensor[batchCount] != 0)
-            detectionLengthTensor[batchCount] += windowLength - 1;
+        if(detectBegin != 0 && detectEnd != 0)
+        {
+            Rpp32s newBegin = std::max<int>(detectBegin - (windowLength - 1), 0);
+            detectEnd += detectBegin - newBegin;
+            detectBegin = newBegin;
+        }
+
+        detectedIndexTensor[batchCount] = detectBegin;
+        detectionLengthTensor[batchCount] = detectEnd;
     }
 
     return RPP_SUCCESS;
